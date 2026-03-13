3e2296b3111f56ffe1718bf7f541fddfaf8c157b
Dugald Topshee <623544+dtopshee@users.noreply.github.com>
2026-03-13 15:02:13 -0400

    Create LegislationFileInfo.cs

---
+++ Models/LegislationFileInfo.cs
@@ -0,0 +1,9 @@
+// Models/LegislationFileInfo.cs
+namespace LegislationTimeMachine.Models
+{
+    public class LegislationFileInfo
+    {
+        public string FileName { get; set; } = string.Empty;
+        public string ShortTitle { get; set; } = string.Empty;
+    }
+}
