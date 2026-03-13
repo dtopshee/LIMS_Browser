d84031dadb1099a619ad468143c4e304dec98be1
Dugald Topshee <623544+dtopshee@users.noreply.github.com>
2026-03-13 15:23:29 -0400

    Update DiffEngine.cs to use var

--- Services/DiffEngine.cs
+++ Services/DiffEngine.cs
@@ -4,40 +4,39 @@ namespace LegislationTimeMachine.Services
 {
     public static class DiffEngine
     {
-        public static Dictionary<string, LegislativeDelta> Compare(List<LegislativeNode> oldVersion, List<LegislativeNode> newVersion)
+       public static Dictionary<string, LegislativeDelta> Compare(List<LegislativeNode> oldVersion, List<LegislativeNode> newVersion)
+{
+    var results = new Dictionary<string, LegislativeDelta>();
+    var oldFlat = Flatten(oldVersion);
+    var newFlat = Flatten(newVersion);
+
+    // Check for additions and modifications
+    foreach (var entry in newFlat) // changed 'newNode' to 'entry'
+    {
+        var fid = entry.Key;
+        var newNode = entry.Value;
+
+        if (!oldFlat.TryGetValue(fid, out var oldNode))
         {
-            var results = new Dictionary<string, LegislativeDelta>();
-
-            // Flatten the trees into dictionaries for easier lookup by FID
-            var oldFlat = Flatten(oldVersion);
-            var newFlat = Flatten(newVersion);
-
-            // Check for additions and modifications
-            foreach (var newNode in newFlat)
-            {
-                if (!oldFlat.TryGetValue(newNode.Key, out var oldNode))
-                {
-                    // It exists in the new version but not the old one
-                    results[newNode.Key] = new LegislativeDelta { Fid = newNode.Key, Type = ChangeType.Added };
-                }
-                else if (oldNode.Content != newNode.Value.Content || oldNode.Label != newNode.Value.Label)
-                {
-                    // It exists in both, but the text or structure has changed
-                    results[newNode.Key] = new LegislativeDelta { Fid = newNode.Key, Type = ChangeType.Modified };
-                }
-            }
-
-            // Check for removals
-            foreach (var oldNode in oldFlat)
-            {
-                if (newNode.Value.RepealDate.HasValue && newNode.Value.RepealDate <= targetDate)
-                {
-                    results[newNode.Key] = new LegislativeDelta { Fid = newNode.Key, Type = ChangeType.Removed };
-                }
-            }
-
-            return results;
+            results[fid] = new LegislativeDelta { Fid = fid, Type = ChangeType.Added };
         }
+        else if (oldNode.Content != newNode.Content || oldNode.Label != newNode.Label || oldNode.MarginalNote != newNode.MarginalNote)
+        {
+            results[fid] = new LegislativeDelta { Fid = fid, Type = ChangeType.Modified };
+        }
+    }
+
+    // Check for removals
+    foreach (var entry in oldFlat)
+    {
+        if (!newFlat.ContainsKey(entry.Key))
+        {
+            results[entry.Key] = new LegislativeDelta { Fid = entry.Key, Type = ChangeType.Removed };
+        }
+    }
+
+    return results;
+}
 
         private static Dictionary<string, LegislativeNode> Flatten(List<LegislativeNode> nodes)
         {
